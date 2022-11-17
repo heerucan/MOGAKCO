8719464fbbbae98dde24600cbcba95d984bee1c4
heerucan <iwpow0307@gmail.com>
2022-11-18 01:31:15 +0900

    [#21] 사용자 위치 권한 허용 - info.plist

---
+++ MOGAKCO/MOGAKCO/Global/DelegateProxy/CoreLocationDelegateProxy.swift
@@ -0,0 +1,8 @@
+//
+//  CoreLocationProxy.swift
+//  MOGAKCO
+//
+//  Created by heerucan on 2022/11/18.
+//
+
+import Foundation
--- MOGAKCO/MOGAKCO/Support/SceneDelegate.swift
+++ MOGAKCO/MOGAKCO/Support/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let vc = SplashViewController()
+        let vc = HomeViewController()
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
     }
